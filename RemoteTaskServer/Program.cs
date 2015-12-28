723dc857decf49406298dcc394cf17440b8dd33f
Andrew <andrew@codeusa523.org>
2015-12-28 18:17:02 -0500

    Write savings to disk if they don't exist

--- RemoteTaskServer/Program.cs
+++ RemoteTaskServer/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Security.Principal;
 using RemoteTaskServer.Api;
 using RemoteTaskServer.Server;
@@ -9,6 +10,7 @@ using RemoteTaskServer.Utilities;
 using RemoteTaskServer.Utilities.Network;
 using RemoteTaskServer.Utilities.System;
 using RemoteTaskServer.WebServer;
+using UlteriusServer.Properties;
 
 #endregion
 
@@ -22,11 +24,19 @@ namespace RemoteTaskServer
                 ExceptionHandler.AddGlobalHandlers();
 
             var settings = new Settings();
+            if (!File.Exists("UlteriusServer.ini"))
+            {
+                Console.WriteLine(Resources.Program_Main_Settings_didn_t_exist__writing_to_disk_);
+                byte[] bytes = new byte[Resources.UlteriusServer.Length * sizeof(char)];
+                System.Buffer.BlockCopy(Resources.UlteriusServer.ToCharArray(), 0, bytes, 0, bytes.Length);
+                File.WriteAllBytes("UlteriusServer.ini", bytes);
+            }
+
 
             var myPrincipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
             if (myPrincipal.IsInRole(WindowsBuiltInRole.Administrator) == false)
             {
-                Console.WriteLine("Its recommended You need to elevate this server to administrator.");
+                Console.WriteLine(Resources.Program_Main_Its_recommended_You_need_to_elevate_this_server_to_administrator_);
             }
 
             var useWebServer = settings.Read("UseWebServer", "WebServer");
@@ -35,12 +45,12 @@ namespace RemoteTaskServer
                 var root = settings.Read("WebFilePath", "WebServer");
                 var port = int.Parse(settings.Read("WebServerPort", "WebServer"));
                 var httpServer = new HttpServer(root, port);
-                Console.WriteLine("Web Server is running on this port: " + httpServer.Port);
+                Console.WriteLine(Resources.Program_Main_Web_Server_is_running_on_this_port__ + httpServer.Port);
             }
 
-            Console.Title = "Strike Task Manager";
+            Console.Title = Resources.Program_Title;
             TaskServer.Start();
-            Console.WriteLine("Starting TaskServer on " + NetworkUtilities.GetIPv4Address() + ":" + TaskServer.boundPort);
+            Console.WriteLine(Resources.Program_Main_Starting_TaskServer_on_ + NetworkUtilities.GetIPv4Address() + ":" + TaskServer.boundPort);
             var systemUtilities = new SystemUtilities();
             systemUtilities.Start();
             TaskApi.RestartServer();
