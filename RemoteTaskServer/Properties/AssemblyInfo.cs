fd6fd44a1dfa836df15798adbbaaf8171a22270d
Andrew Sampson <andrew@codeusa523.org>
2016-08-29 21:42:26 -0400

    Update assembly

--- RemoteTaskServer/Properties/AssemblyInfo.cs
+++ RemoteTaskServer/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using log4net.Config;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 [assembly: XmlConfigurator]
