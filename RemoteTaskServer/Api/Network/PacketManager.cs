b8c2a6a4764e724fcb9187dd2ef999d91ecf036f
Andrew Sampson <andrew@codeusa523.org>
2016-09-01 00:56:16 -0400

    Print the packet that throws invalid handshake

--- RemoteTaskServer/Api/Network/PacketManager.cs
+++ RemoteTaskServer/Api/Network/PacketManager.cs
@@ -126,6 +126,7 @@ namespace UlteriusServer.Api.Network
                     if (!endpoint.Equals("aeshandshake") || authClient.AesShook)
                     {
                         Console.WriteLine("Invalid handshake protocol");
+                        Console.WriteLine(packetData);
                         _packetType = PacketTypes.InvalidOrEmptyPacket;
                         return;
                     }
